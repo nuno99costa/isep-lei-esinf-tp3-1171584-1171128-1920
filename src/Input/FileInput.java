1889bd66b13e35432cdf6c4d2be6870457f728a4
Andre99Rainho <1171128@isep.ipp.pt>
2019-11-26 15:45:58 +0000

    readDataFiles method to upload given information

--- src/Input/FileInput.java
+++ src/Input/FileInput.java
@@ -1,32 +1,25 @@
 package Input;
 
-import graphbase.Border;
-import graphbase.Graph;
+import treebase.AVL;
 import treebase.Country;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileInput {
 
-    /**
-     * Retrives all the countries and borders from the given files and uses them
-     * to build the given graph with the countries as the vertexes and borders
-     * as the edges
-     *
-     * @param countryMap graph to store all the files' information
-     * @throws FileNotFoundException
-     */
-    public static void readDataFiles(Graph countryMap) throws FileNotFoundException {
+    public static void readDataFiles(bstree givenTree) throws FileNotFoundException
+    {
+        AVL tree = givenTree.getTree();
         Scanner countryInput = new Scanner(new File("paises.txt"));
         Scanner borderInput = new Scanner(new File("fronteiras.txt"));
-        ArrayList<Country> countries = new ArrayList<>();
 
-        while (countryInput.hasNextLine()) {
+        while (countryInput.hasNextLine())
+        {
             String line = countryInput.nextLine();
-            if (line.trim().length() > 0) {
+            if (line.trim().length() > 0)
+            {
                 String[] content = line.split(",");
                 String name = content[0].trim();
                 String continent = content[1].trim();
@@ -34,62 +27,30 @@ public class FileInput {
                 String capital = content[3].trim();
                 double latitude = Double.parseDouble(content[4]);
                 double longitude = Double.parseDouble(content[5]);
-                Country temp = new Country(name, continent, population, capital, latitude, longitude);
-                countries.add(temp);
-                countryMap.insertVertex(temp);
-            }
-        }
 
-        Country countryOrigin = null;
-        Country countryDestination = null;
+                Country cont = new Country(name, continent, population, capital, latitude, longitude);
 
-        while (borderInput.hasNextLine()) {
-            String line = borderInput.nextLine();
-            if (line.trim().length() > 0) {
-                String[] content = line.split(",");
-                String c1 = content[0].trim();
-                String c2 = content[1].trim();
+                String borderLine = borderInput.nextLine();
+                if (borderLine.trim().length() > 0)
+                {
+                    String[] borderContent = borderLine.split(",");
+                    String border1 = borderContent[0].trim();
+                    String border2 = borderContent[1].trim();
 
-                for (Country var : countries) {
-                    String countryName = var.getName().trim();
-                    if (countryName.equals(c1)) {
-                        countryOrigin = var;
+                    if (name.equals(border1))
+                    {
+                        cont.addBorder(border2);
                     }
-                    if (countryName.equals(c2)) {
-                        countryDestination = var;
+
+                    if (name.equals(border2))
+                    {
+                        cont.addBorder(border1);
                     }
                 }
-                Border edgeBorder = new Border(countryOrigin, countryDestination);
-                double edgeDistance = distance(countryOrigin, countryDestination);
-                countryMap.insertEdge(countryOrigin, countryDestination, edgeBorder, edgeDistance);
+                tree.insert(cont);
             }
-        }
-    }
-
-    /**
-     * Given two Country objects, calculates the distance between them in
-     * kilometers (km)
-     *
-     * @param contOrigin first Country object
-     * @param contDest   second Country object
-     * @return double value containing countries' distance in km
-     */
-    private static double distance(Country contOrigin, Country contDest) {
-
-        double lat1 = contOrigin.getLatitude();
-        double lon1 = contOrigin.getLongitude();
-        double lat2 = contDest.getLatitude();
-        double lon2 = contDest.getLongitude();
-
-        if ((lat1 == lat2) && (lon1 == lon2)) {
-            return 0;
-        } else {
-            double theta = lon1 - lon2;
-            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-            dist = Math.acos(dist);
-            dist = Math.toDegrees(dist);
-            dist = dist * 60 * 1.1515 * 1.609344;
-            return (dist);
         }
     }
 }
+
+
