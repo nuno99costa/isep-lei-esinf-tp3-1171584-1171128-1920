ee9077e54dc0458609c75aeade90bb11cc2d0931
Andre99Rainho <1171128@isep.ipp.pt>
2019-11-29 17:14:02 +0000

    Creation of KDTTest JUnit test class

---
+++ test/treebase/KDTTest.java
@@ -0,0 +1,36 @@
+package treebase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KDTTest {
+
+    @Test
+    public void addComparator() {
+    }
+
+    @Test
+    public void getComparator() {
+    }
+
+    @Test
+    public void defineBoundingBoxes() {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void posOrderNode() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
