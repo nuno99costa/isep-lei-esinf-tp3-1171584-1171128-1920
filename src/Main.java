821512b04610ec1db2c25580ba14bd928e36a29b
Andre99Rainho <1171128@isep.ipp.pt>
2019-12-01 17:46:18 +0000

    Functional Main

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ import treebase.LatitudeLongitude2DTree;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner read = new Scanner(System.in);
         AlphabeticalCountryTree ex1tree = new AlphabeticalCountryTree();
         FileInput.readDataFiles(ex1tree.getTree());
 
@@ -18,17 +20,55 @@ public class Main {
         System.out.println("Fronteiras de Brasil: " + ex1tree.getBorders("brasil"));
         System.out.println("Fronteiras de Alemanha: " + ex1tree.getBorders("alemanha"));
 
-        //Ex2
+        System.out.println("\nPrima qualquer tecla para proceder");
+        read.next();
+        System.out.println();
+
+        //Ex1.b
+        System.out.println("Exercício 1-B \n");
+        System.out.println("Países da Europa: " + ex1tree.getOrderedInContinent("europa"));
+        System.out.println("Países da América do Sul: " + ex1tree.getOrderedInContinent("americasul"));
+
+        System.out.println("\nPrima qualquer tecla para proceder");
+        read.next();
+        System.out.println();
+
+        //Ex2.a
         LatitudeLongitude2DTree ex2tree = new LatitudeLongitude2DTree();
         FileInput.readDataFiles(ex2tree.getTree());
 
-        System.out.println(ex2tree.getTree());
+        //Ex2.b
+        System.out.println("Exercício 2-B \n");
+        double lat = 52.5234051;
+        double lon = 13.4113999;
+        System.out.println("Latitude: " + lat + "\nLongitude: " + lon);
+        System.out.println("País: " + ex2tree.exactFind(lat, lon));
 
-        Country test = ex2tree.nearestNeighbor(84, -11);
-        System.out.println(test);
+        System.out.println("\nPrima qualquer tecla para proceder");
+        read.next();
+        System.out.println();
 
-        ArrayList test1 = (ArrayList) ex2tree.getCountriesInsideGeoBox(6, -67, 11, -57);
-        System.out.println(test1);
+        //Ex2.c
+        System.out.println("Exercício 2-C \n");
+        double latitude = 84;
+        double longitude = -11;
+        System.out.println("Latitude: " + latitude + "\nLongitude: " + longitude);
+        System.out.println("País mais próximo: " + ex2tree.nearestNeighbor(latitude, longitude));
+
+        System.out.println("\nPrima qualquer tecla para proceder");
+        read.next();
+        System.out.println();
+
+        //Ex2.d
+        System.out.println("Exercício 2-D \n");
+        double latitude1 = 6;
+        double latitude2 = -67;
+        double longitude1 = 11;
+        double longitude2 = -57;
+        System.out.println("Latitude 1: " + latitude1 + "\nLongitude1: " + longitude1);
+        System.out.println("Latitude 2: " + latitude2 + "\nLongitude2: " + longitude2);
+        System.out.println("Países: " + ex2tree.getCountriesInsideGeoBox(latitude1, longitude1, latitude2, longitude2));
+
     }
 
 }
